ece196c06f1f1ddbf64cda7ae85036a6199697ba
Joao Francisco <joaofrancisco21@yahoo.com.br>
2024-10-22 22:02:11 -0300

    first commit

---
+++ src/app/(website)/page.tsx
@@ -0,0 +1,7 @@
+import Home from "@/components/(website)/home";
+
+export default function Page() {
+  return (
+    <Home/>
+  );
+}
---
+++ src/components/(auth)/login/index.tsx
@@ -0,0 +1,7 @@
+import { Heading } from "@chakra-ui/react";
+
+export default function LoginPage() {
+  return (
+    <Heading>Hello World!</Heading>
+  );
+}
