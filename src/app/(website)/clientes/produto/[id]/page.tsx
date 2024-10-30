c62b2b52cf53529689eb5e7916d3bce1271292db
João Francisco Perassoli Lopes <joao.lopes@gazin.com.br>
2024-10-30 16:39:23 -0300

    feat(GINOVEI-16): Add structure

---
+++ src/app/(website)/clientes/produto/[id]/page.tsx
@@ -0,0 +1,7 @@
+import SelectedProduct from "@/components/(website)/clientes/produto";
+
+export default function Page(): JSX.Element{
+    return(
+        <SelectedProduct/>
+    );
+}
---
+++ src/components/(website)/clientes/produto/index.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+import { Heading } from "@chakra-ui/react";
+
+export default function SelectedProduct(): JSX.Element{
+    return(
+        <Heading>Hello World!</Heading>
+    );
+}
